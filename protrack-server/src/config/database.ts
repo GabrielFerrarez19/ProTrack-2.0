e588f8e11184bbca6f86e345c0e01f0ed21c65c2
Gabriel Cristiano Ferraez <134634881+GabrielFerrarez19@users.noreply.github.com>
2025-08-14 20:03:21 -0300

    Update database.ts

--- protrack-server/src/config/database.ts
+++ protrack-server/src/config/database.ts
@@ -1,9 +1,9 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: "caboose.proxy.rlwy.net",
-  user: "root",
-  password: "BOwuyhBlulgOsPBiNVszpLGGmPQceMaH",
-  port: 44969,
-  database: "protrack", // igual ao final da URL
+  host: "localhost",      // conecta na própria EC2
+  user: "root",           // usuário que você configurou
+  password: "gabri1234",  // senha definida
+  port: 3306,             // porta padrão do MySQL
+  database: "protrack",   // banco que você criou
 });
