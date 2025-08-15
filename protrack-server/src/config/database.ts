17b6cbde7ae29728857c666b595c83b9b13940c6
Ubuntu <ubuntu@ip-172-31-46-115.us-east-2.compute.internal>
2025-08-15 20:12:38 +0000

    awd

--- protrack-server/src/config/database.ts
+++ protrack-server/src/config/database.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: "127.0.0.1", // ou '18.191.70.76' se for remoto
+  host: "18.191.70.76", // ou '18.191.70.76' se for remoto
   user: "protrack_user",
   password: "senha_segura_aqui",
   database: "protrack",
