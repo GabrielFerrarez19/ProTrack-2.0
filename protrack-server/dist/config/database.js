cdf65c682d8a313b604fa058bc0b09ba1649756a
GabrielFerrarez19 <gabrielferrarez190206@gmail.com>
2025-08-15 16:06:55 -0300

    ArrumaTUdoAgorapF

--- protrack-server/dist/config/database.js
+++ protrack-server/dist/config/database.js
@@ -4,11 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
+// db.ts
 const promise_1 = __importDefault(require("mysql2/promise"));
-exports.db = promise_1.default.createPool({
-    host: "localhost", // conecta na própria EC2
-    user: "root", // usuário que você configurou
-    password: "gabri1234", // senha definida
-    port: 3306, // porta padrão do MySQL
-    database: "protrack", // banco que você criou
-});
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+let db;
+if (process.env.DATABASE_URL) {
+    // Se DATABASE_URL estiver definida, conecta usando ela
+    exports.db = db = promise_1.default.createPool(process.env.DATABASE_URL);
+}
+else {
+    // Caso contrário, usa variáveis separadas
+    exports.db = db = promise_1.default.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_DATABASE,
+    });
+}
--- protrack-server/src/config/database.ts
+++ protrack-server/src/config/database.ts
@@ -1,9 +1,23 @@
+// db.ts
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-export const db = mysql.createPool({
-  host: "localhost",      // conecta na própria EC2
-  user: "root",           // usuário que você configurou
-  password: "gabri1234",  // senha definida
-  port: 3306,             // porta padrão do MySQL
-  database: "protrack",   // banco que você criou
-});
+dotenv.config();
+
+let db;
+
+if (process.env.DATABASE_URL) {
+  // Se DATABASE_URL estiver definida, conecta usando ela
+  db = mysql.createPool(process.env.DATABASE_URL);
+} else {
+  // Caso contrário, usa variáveis separadas
+  db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+  });
+}
+
+export { db };
