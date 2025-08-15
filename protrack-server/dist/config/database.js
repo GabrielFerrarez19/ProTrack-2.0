39f1b9a7c7229e52a9156bfdfeb972f400fd1670
GabrielFerrarez19 <gabrielferrarez190206@gmail.com>
2025-08-15 16:39:24 -0300

    aw

--- protrack-server/dist/config/database.js
+++ protrack-server/dist/config/database.js
@@ -8,18 +8,10 @@ exports.db = void 0;
 const promise_1 = __importDefault(require("mysql2/promise"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-let db;
-if (process.env.DATABASE_URL) {
-    // Se DATABASE_URL estiver definida, conecta usando ela
-    exports.db = db = promise_1.default.createPool(process.env.DATABASE_URL);
-}
-else {
-    // Caso contrário, usa variáveis separadas
-    exports.db = db = promise_1.default.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        port: Number(process.env.DB_PORT),
-        database: process.env.DB_DATABASE,
-    });
-}
+exports.db = promise_1.default.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+});
--- protrack-server/src/config/database.ts
+++ protrack-server/src/config/database.ts
@@ -1,23 +1,12 @@
 // db.ts
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-let db;
-
-if (process.env.DATABASE_URL) {
-  // Se DATABASE_URL estiver definida, conecta usando ela
-  db = mysql.createPool(process.env.DATABASE_URL);
-} else {
-  // Caso contrário, usa variáveis separadas
-  db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_DATABASE,
-  });
-}
-
-export { db };
+export const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_DATABASE,
+});
