f0ced0ea61539d10b585f63521f2d3ad324b3cb3
GabrielFerrarez19 <gabrieferrarez190206@gmail.com>
2026-02-12 10:18:35 -0300

    feat: campo category_name adicionado

--- protrack-server/internal/products/domain/products_domain.go
+++ protrack-server/internal/products/domain/products_domain.go
@@ -82,6 +82,26 @@ type ProductResponse struct {
 	DeletedAt   time.Time `json:"deleted_at"`
 }
 
+type ListProductsByCompanyRow struct {
+	ID           uuid.UUID `json:"id"`
+	CompanyID    uuid.UUID `json:"company_id"`
+	CategoryID   uuid.UUID `json:"category_id"`
+	Name         string    `json:"name"`
+	Description  string    `json:"description"`
+	Barcode      string    `json:"barcode"`
+	Quantity     int32     `json:"quantity"`
+	Size         string    `json:"size"`
+	CostPrice    float64   `json:"cost_price"`
+	SalePrice    float64   `json:"sale_price"`
+	CreatedBy    uuid.UUID `json:"created_by"`
+	UpdatedBy    uuid.UUID `json:"updated_by"`
+	DeletedBy    uuid.UUID `json:"deleted_by"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	DeletedAt    time.Time `json:"deleted_at"`
+	CategoryName string    `json:"category_name"`
+}
+
 func ApplyUpdateProductCategoryParams(
 	req UpdateProductRequest,
 	arg *db.UpdateProductParams,
