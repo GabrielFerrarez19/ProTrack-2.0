397f1ecdc2c8b26653bafb4cf31c973e2f903b9a
Gabriel Cristiano Ferraez <gabrielferrarez190206@gmail.com>
2026-02-17 19:08:04 -0300

    feat: handler finalizado sem error de build

---
+++ protrack-server/internal/sales/handler/sales.route.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"github.com/GabrielFerrarez19/ProTrack-2.0/protrack-server/internal/adapters/http/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) RegisterRoute(r *gin.RouterGroup){
+	sales := r.Group("/sales").Use(middleware.AuthMiddleware(h.jwtManager))
+	{
+		sales.POST("", h.CreateSale)
+		sales.DELETE("/:id", h.DeleteSale)
+		sales.GET("/:id", h.GetSaleById)
+		sales.GET("/list/", h.ListSales)
+		sales.PUT("/status/:id", h.UpdateSaleStatus)
+	}
+}
