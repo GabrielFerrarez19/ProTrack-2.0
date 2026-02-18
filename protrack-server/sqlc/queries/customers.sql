2e52318f301b64551f06ebc04a5e18489d9d243e
GabrielFerrarez19 <gabrieferrarez190206@gmail.com>
2026-02-18 17:15:51 -0300

    feat: nova logica por metodo de pagamento

--- protrack-server/sqlc/queries/customers.sql
+++ protrack-server/sqlc/queries/customers.sql
@@ -88,7 +88,7 @@ WHERE id = $1
     AND deleted_at IS NULL;
 -- name: UpdateBalanceDueCustomer :exec
 UPDATE customers
-SET balance_due = $2,
+SET balance_due = balance_due + $2,
     updated_by = $3,
     updated_at = CURRENT_TIMESTAMP
 WHERE id = $1
