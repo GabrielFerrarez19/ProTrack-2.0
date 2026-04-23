c9bba8f2ac01a0bac234f6d8af0338e883438647
GabrielFerrarez19 <gabrielferrarez190206@gmail.com>
2026-04-23 20:17:23 -0300

    feat: corrigindo o dialog de componente

--- proTrack-client/src/pages/Status/index.tsx
+++ proTrack-client/src/pages/Status/index.tsx
@@ -1,28 +1,11 @@
-import { CompanyRegistrationModal } from "@/components/DioalogCompanyRegistration";
 import { Header } from "../../components/header";
 import { CardsClientes } from "./CardsClientes";
 import { CardsStatus } from "./CardsStatus";
 import { TableData } from "./TabelaDados";
-import { useAuth } from "@/hooks/useAuth";
-import { useEffect, useState } from "react";
 import { useStatus } from "@/hooks/useStatus";
 
 export function Status() {
-  const { hasCompany, setHasCompany } = useAuth();
-  const [openModal, setOpenModal] = useState(false);
   const { dados, loading, error } = useStatus();
-
-  useEffect(() => {
-    if (!hasCompany) {
-      setOpenModal(true);
-    }
-  }, [hasCompany]);
-
-  const handleRegistrationComplete = () => {
-    setHasCompany(true);
-    localStorage.setItem("has_company", "true");
-    setOpenModal(false);
-  };
 
   return (
     <div className="p-6 space-y-6 w-full">
@@ -38,11 +21,6 @@ export function Status() {
       <CardsClientes />
       {/* Table */}
       <TableData />
-
-      <CompanyRegistrationModal
-        open={openModal}
-        onComplete={handleRegistrationComplete}
-      />
     </div>
   );
 }
